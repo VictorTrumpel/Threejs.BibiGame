fd8d3030fe0c97da10f85de2fc2cfdad8305a32d
victor <>
2022-03-17 10:48:52 +0300

    [+] add Readme

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,6 @@ import '../interface/index.scss';
 
 import './loaders/bibi';
 import './loaders/enemy';
-// import './loaders/tigro';
 
 import { Ground } from './environment/common/Ground';
 import { Light } from './environment/common/Light';
